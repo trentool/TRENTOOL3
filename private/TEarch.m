dd8851e68a2d761d3e698d8ff20f0e4366b9c317
trentool <p.wollstadt@stud.uni-frankfurt.de>
2015-03-20 14:05:42 +0100

    TEarch.m now recognizes Mac 64bit

--- private/TEarch.m
+++ private/TEarch.m
@@ -8,31 +8,37 @@ function [path2TSTOOL] = TEarch
 %
 % PW 10/11/2014
 
+% CHANGELOG
+% 2015/02/12 - PW: added support for newer MAC versions (.mexmaci64)
+
 % check if TSTool functions are already on the path
 h = which('nn_prepare.m');
 if ~isempty(h)
     return
 end
 
-arch = computer('arch');
 path2TRENTOOL = which('TEprepare');
 path2TRENTOOL = path2TRENTOOL(1:end-11);
+path2TSTOOL = fullfile(path2TRENTOOL,'tstool_functions');
 
-% check OS
-if strcmp(arch(1:3),'win')    
-    path2TSTOOL = fullfile(path2TRENTOOL,'tstool_functions','mex_win');
-else 
-    path2TSTOOL = fullfile(path2TRENTOOL,'tstool_functions','mex_linux');
+extension = mexext;		% gets the mex extension expected by the current architecture
+
+switch extension
+  case 'mexw64'
+    path2TSTOOL_mex = fullfile(path2TSTOOL,'mex_win', 'mex64');
+  case 'mexw32'
+    path2TSTOOL_mex = fullfile(path2TSTOOL,'mex_win', 'mex32');
+  case 'mexmaci64'
+    path2TSTOOL_mex = fullfile(path2TSTOOL,'mex_mac', 'mex64');
+  case 'mexmaci32'
+    path2TSTOOL_mex = fullfile(path2TSTOOL,'mex_mac', 'mex32');
+    case 'mexa64'
+    path2TSTOOL_mex = fullfile(path2TSTOOL,'mex_linux', 'mex64');
+  case 'mexglx' 
+    path2TSTOOL_mex = fullfile(path2TSTOOL,'mex_linux', 'mex32');
+  otherwise
+    error(['TRENTOOL ERROR: System not supported! Expected extension: ''' mexext '''']);
 end
 
-addpath(path2TSTOOL);   % this folder contains the help files for TSTool mex functions
-
-% check 32 vs. 64 bit
-if strcmp(arch(end-1:end), '64')
-    path2TSTOOL_mex = fullfile(path2TSTOOL, 'mex64');
-else
-    path2TSTOOL_mex = fullfile(path2TSTOOL, 'mex32');
-end
-    
-
+addpath(path2TSTOOL);       % this folder contains the help files for TSTool mex functions
 addpath(path2TSTOOL_mex);   % this folder contains the mex functions themselves
